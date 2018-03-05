6c948a3bdffe64c86e434881a0cb234b1a5e5955
Surya Setiyaputra <hello@setiyaputra.me>
2018-03-05 13:01:23 +1100

    test how WP theme loop looks like

--- footer.php
+++ footer.php
@@ -1,3 +1,9 @@
+</div> <!-- site content -->
+
+<div id='site-footer'>
+<p>&copy; My Awesome Theme</p>
+</div>
+
 <?php wp_footer() ?>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php get_header() ?>
 
-My Awesome Theme
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque quo aut ipsa tempore enim temporibus mollitia amet sunt dolorum, consequatur eveniet dolore vitae? Recusandae provident dolore sequi, minus eum porro.</p>
+
+<?php if( have_posts() ) : ?>
+	<?php while( have_posts() ) : the_post() ?>
+		<h2><a href='<?php the_permalink() ?>'><?php the_title() ?></a></h2>
+		<div class="content">
+			<?php the_content() ?>
+		</div>
+	<?php endwhile ?>
+<?php else : ?>
+	<p>Oh No, there are no posts!</p>
+<?php endif ?>
 
 <?php get_footer() ?>
